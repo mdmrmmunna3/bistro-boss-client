447deaa87e509741d565936d4d016e0cfd4f20dc
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-01-07 19:40:32 +0600

    use stripe test card and display card error method added

--- src/Pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,9 +1,11 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 
 const CheckOutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const [paymentCardError, setPaymentCardError] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -16,29 +18,56 @@ const CheckOutForm = () => {
         if (card === null) {
             return;
         }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+        if (error) {
+            console.log('error', error);
+            setPaymentCardError(error.message);
+        }
+        else {
+            setPaymentCardError('');
+            console.log('payment method', paymentMethod);
+        }
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button
+                    style={
+                        {
+                            background: `linear-gradient(90deg, #835D23 0%, #B58130 100%)`,
+
+                        }
+                    }
+                    className="my-4 text-white px-6 py-1 rounded-sm "
+                    type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+
+            {
+                paymentCardError && <p className="text-red-600">{paymentCardError}</p>
+            }
+        </>
     );
 };
 
---
+++ src/Pages/Dashboard/Payment/Payment_intregation_steos.js
@@ -0,0 +1,6 @@
+/**
+ * 1. install stripe and react stripe js
+ * 2. create checkout form with card element (card element contains: card number, expiration date, zip code)
+ * 3. create stripe account and use publishable key pk
+ * 4. use test card and display card error
+ */
