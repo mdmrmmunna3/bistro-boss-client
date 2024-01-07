7a87050dc5a54ed3406ab9d7aa5e5dd0f6150438
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-01-07 15:58:15 +0600

    create stripe account and use publishable key

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,8 +1,12 @@
 import { Helmet } from "react-helmet-async";
 import HeadingTitel from "../../../components/HeadingTitel/HeadingTitel";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "./CheckOutForm";
 
 
 const Payment = () => {
+    const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK)
     return (
         <div className="md:w-3/4 w-full">
             <Helmet>
@@ -10,6 +14,10 @@ const Payment = () => {
             </Helmet>
             {/* common heading  */}
             <HeadingTitel subHeading="Please Process" heading="Payment"></HeadingTitel>
+
+            <Elements stripe={stripePromise}>
+                <CheckOutForm></CheckOutForm>
+            </Elements>
         </div>
     );
 };
