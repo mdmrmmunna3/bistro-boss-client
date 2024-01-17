38ca60b6795c7ef970dcb0bdeb47ebb2dd9f4fa4
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-01-17 19:30:27 +0600

    load dashboard admin stat using useqyery

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,9 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 
 const AdminHome = () => {
+    const { user } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+
+    const { data: stats = {} } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure('/admin-stats');
+            return res.data;
+        }
+    })
     return (
-        <div>
-            <h2 className="text-4xl">Admin Home</h2>
+        <div className=" w-full">
+            <h2 className="text-4xl">Hi, {user?.displayName} </h2>
         </div>
     );
 };
